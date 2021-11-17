d8e604a43abfa028ebc5eb47ebb6f762eee57073
Jeff Schofield <jeff@jeffschofield.com>
2021-11-17 01:46:59 -0500

    refactor: Export enum constants

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { WLEDClient } from './client'
 
 export default WLEDClient
+export { WLEDNightlightMode, WLEDLiveDataOverride } from './constants'
 export * from './types.wled'
 export * from './types.client'
