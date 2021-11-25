9a96b184e791b097aacc7eddabbfa97d7f80f99b
Jeff Schofield <jeff@jeffschofield.com>
2021-11-24 19:15:30 -0500

    feat: Add `RequireAtLeastOne` utility type

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,8 @@
 export type RGBColor = [number,number,number]
 export type RGBWColor = [number,number,number,number]
+
+// Credit: KPD
+// Source: https://stackoverflow.com/questions/40510611/typescript-interface-require-one-of-two-properties-to-exist/49725198#49725198
+export type RequireAtLeastOne<T, Keys extends keyof T = keyof T> =
+							Pick<T, Exclude<keyof T, Keys>> &
+							{ [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>> }[Keys]
